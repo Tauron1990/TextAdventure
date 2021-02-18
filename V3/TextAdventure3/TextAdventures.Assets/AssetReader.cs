b90ce07d9b080784302d3cbdca58bdd0fc83d665
Alexander Bachmaier <Tauron.ab@gmail.com>
2021-02-18 06:37:35 +0100

    Commit

---
+++ V3/TextAdventure3/TextAdventures.Assets/AssetReader.cs
@@ -0,0 +1,11 @@
+﻿namespace TextAdventures.Assets
+{
+    public sealed class AssetReader
+    {
+        private readonly string _assetDictionary;
+
+        public AssetReader(string assetDictionary) => _assetDictionary = assetDictionary;
+
+        
+    }
+}
